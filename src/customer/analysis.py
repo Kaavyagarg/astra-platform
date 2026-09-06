9f7434df029c5ec29f4784d69e26e606ef8f9749
Kavya Garg <garg.kavya99kg@gmail.com>
2026-09-06 19:20:59 +0530

    Added functionalities in analysis.py and tested the same

--- src/customer/analysis.py
+++ src/customer/analysis.py
@@ -8,3 +8,29 @@ customers = [
     {"name": "Bob", "age": "17"},
 ]
 print(get_customer_names(customers))
+
+def get_customer_positions(customers):
+    list_name = []
+    for i, customer in enumerate(customers):
+        list_name.append((i,customer['name']))
+    return list_name
+customers = [
+    {"name": "Alice", "age": "25"},
+    {"name": "Bob", "age": "17"},
+]
+print(get_customer_positions(customers))
+
+def sort_customers_by_age(customers):
+    return sorted(customers, key = lambda customer:customer["age"])
+
+def count_age_groups(customers):
+    counts = {
+        "adult":0,
+        "minor":0
+    }
+    for customer in customers:
+        if customer["age"] >= 18:
+            counts["adult"] += 1
+        else:
+            counts["minor"] += 1
+    return counts 
--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,7 +1,34 @@
-from customer.analysis import get_customer_names
+from customer.analysis import get_customer_names, get_customer_positions,sort_customers_by_age,count_age_groups
 def test_get_customer_names():
     customers = [
         {"name":"Alice","age":"25"},
         {"name":"Bob","age":"17"}
     ]
     assert get_customer_names(customers) == ["Alice","Bob"]
+
+def test_get_customer_positions():
+    customers = [
+            {"name":"Alice","age":"25"},
+            {"name":"Bob","age":"17"}
+        ]
+    assert get_customer_positions(customers) == [(0,"Alice"),(1,"Bob")]
+
+def test_sort_customers_by_age():
+    customers = [
+        {"name": "Alice", "age": 25},
+        {"name": "Bob", "age": 17},
+        {"name": "Charlie", "age": 35},
+    ]
+    assert sort_customers_by_age(customers) == [
+        {"name":"Bob","age":17},
+        {"name":"Alice","age":25},
+        {"name":"Charlie","age":35}
+    ]
+
+def test_count_age_groups():
+    customers = [
+            {"name": "Alice", "age": 25},
+            {"name": "Bob", "age": 17},
+            {"name": "Charlie", "age": 35},
+        ]
+    assert count_age_groups(customers) == {"adult":2,"minor":1}
